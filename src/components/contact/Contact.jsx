3d14d2e1a3ecc71fc4c6825931f423478e81216b
khoubaib <khoubaib.bnz@gmail.com>
2021-07-18 15:56:44 +0100

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,16 @@ import "./contact.scss"
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-            
+            <div className="p1">
+                <img src="assets/contact.jpg" alt="" />
+            </div>
+            <div className="p2">
+                <form action="">
+                    <input type="email" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
     )
 }
