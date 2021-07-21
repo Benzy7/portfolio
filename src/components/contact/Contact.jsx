1ba7ddfd5cc4705b9e10b7f5b3fa412e907e2726
khoubaib <khoubaib.bnz@gmail.com>
2021-07-21 12:45:29 +0100

    contact(2)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,9 +5,10 @@ export default function Contact() {
     return (
         <div className="contact" id="contact">
             <div className="p1">
-                <img src="assets/contact.jpg" alt="" />
+                <img src="assets/hands-.jpg" alt="" />
             </div>
             <div className="p2">
+                <h2>Contact</h2>
                 <ContactForm />
             </div>
         </div>
